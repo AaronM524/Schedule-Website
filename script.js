086d453ff986308780078df4b666402883a4a310
AaronM524 <aaronmathew468@gmail.com>
2024-10-30 10:37:18 -0400

    done for the day

--- script.js
+++ script.js
@@ -6,13 +6,12 @@ $(document).ready(function () {
       2: { start: '9:36 AM', end: '10:43 AM' },
       3: { start: '10:48 AM', end: '11:55 AM' },
       lunch: { start: '11:55 AM', end: '12:36 PM' }, // Lunch period
-      4: { start: '12:41 PM', end: '1:48 PM' },
-      5: { start: '1:53 PM', end: '3:00 PM' }
+      5: { start: '12:41 PM', end: '1:48 PM' },
+      6: { start: '1:53 PM', end: '3:00 PM' }
   };
 
   btn.on('click', function () {
       const selectedDay = $("#dayInput").val().toUpperCase();
-
       $.ajax({
           url: `https://api.npoint.io/8f7b9e912896b167d39b`,
           method: 'GET',
