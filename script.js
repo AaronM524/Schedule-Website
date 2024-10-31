c619cc45914b57fa7652460eb85a6faa90b661d5
AaronM524 <aaronmathew468@gmail.com>
2024-10-31 11:18:46 -0400

    done with the functionality of the website just need to style it

--- script.js
+++ script.js
@@ -1,58 +1,57 @@
 $(document).ready(function () {
-  const btn = $("#submitDay");
+    const btn = $("#submitDay");
 
-  const bellSchedule = {
-      1: { start: '8:24 AM', end: '9:31 AM' },
-      2: { start: '9:36 AM', end: '10:43 AM' },
-      3: { start: '10:48 AM', end: '11:55 AM' },
-      lunch: { start: '11:55 AM', end: '12:36 PM' }, // Lunch period
-      5: { start: '12:41 PM', end: '1:48 PM' },
-      6: { start: '1:53 PM', end: '3:00 PM' }
-  };
+    const dailyPeriods = {
+        A: [1, 2, 3, "Lunch", 5, 6],
+        B: [4, 1, 2, "Lunch", 7, 5],
+        C: [3, 4, 1, "Lunch", 6, 7],
+        D: [2, 3, 4, "Lunch", 5, 6],
+        E: [1, 2, 3, "Lunch", 7, 5],
+        F: [4, 1, 2, "Lunch", 6, 7],
+        G: [3, 4, 7, "Lunch", 5, 6]
+    };
 
-  btn.on('click', function () {
-      const selectedDay = $("#dayInput").val().toUpperCase();
-      $.ajax({
-          url: `https://api.npoint.io/8f7b9e912896b167d39b`,
-          method: 'GET',
-          success: function (data) {
-              const schedule = data.schedule.filter(item => item.days.includes(selectedDay));
-              
-              if (schedule.length === 0) {
-                  alert("No classes scheduled for this day.");
-              } else {
-                  $("#scheduleList").empty(); // Clear previous data
+    btn.on('click', function () {
+        const selectedDay = $("#dayInput").val().toUpperCase();
+        
+        $.ajax({
+            url: 'https://api.npoint.io/8f7b9e912896b167d39b',
+            method: 'GET',
+            success: function (data) {
+                const schedule = data.schedule;
+                const dayPeriods = dailyPeriods[selectedDay];
+                $("#scheduleList").empty(); 
 
-                  schedule.forEach(item => {
-                      const periodTime = bellSchedule[item.period] || { start: "N/A", end: "N/A" };
-                      const row = `
-                          <tr>
-                              <td>${item.period}</td>
-                              <td>${periodTime.start} - ${periodTime.end}</td>
-                              <td>${item.class}</td>
-                              <td>${item.teacher}</td>
-                              <td>${item.room}</td>
-                          </tr>
-                      `;
-                      $("#scheduleList").append(row);
 
-                      // Insert lunch row after period 3
-                      if (item.period === 3) {
-                          const lunchRow = `
-                              <tr>
-                                  <td>Lunch</td>
-                                  <td>${bellSchedule.lunch.start} - ${bellSchedule.lunch.end}</td>
-                                  <td colspan="3">Lunch Break</td>
-                              </tr>
-                          `;
-                          $("#scheduleList").append(lunchRow);
-                      }
-                  });
-              }
-          },
-          error: function () {
-              alert("There's a connection error.");
-          }
-      });
-  });
+                dayPeriods.forEach(period => {
+                    if (period === "Lunch") {
+                        const lunchRow = `
+                            <tr>
+                                <td>Lunch</td>
+                                <td colspan="3">Lunch Break</td>
+                            </tr>
+                        `;
+                        $("#scheduleList").append(lunchRow);
+                    } else {
+                        const classItem = schedule.find(item => item.period === period && item.days.includes(selectedDay));
+                        
+                        if (classItem) {
+                            const row = `
+                                <tr>
+                                    <td>${classItem.period}</td>
+                                    <td>${classItem.class}</td>
+                                    <td>${classItem.teacher}</td>
+                                    <td>${classItem.room}</td>
+                                </tr>
+                            `;
+                            $("#scheduleList").append(row);
+                    }
+                }
+                });
+            },
+            error: function () {
+                alert("There's a connection error.");
+            }
+        });
+    });
 });
